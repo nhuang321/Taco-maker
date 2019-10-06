d53916f406a0fdf71dc15c0b220605f273105f01
Nancy Huang <nhuang321@berkeley.edu>
2019-10-06 16:20:51 -0700

    Updates

---
+++ precache-manifest.e4606a599d0d5677f452d579f32548e1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f7f2122e5dc7078b3e41a4831bcc4d1",
+    "url": "/nhuang321/Taco-maker/index.html"
+  },
+  {
+    "revision": "afdd11f36c22a0e894bd",
+    "url": "/nhuang321/Taco-maker/static/css/main.fb0e778d.chunk.css"
+  },
+  {
+    "revision": "d886cdb0059fafe34b73",
+    "url": "/nhuang321/Taco-maker/static/js/2.13cd7c3b.chunk.js"
+  },
+  {
+    "revision": "afdd11f36c22a0e894bd",
+    "url": "/nhuang321/Taco-maker/static/js/main.0fb05f1b.chunk.js"
+  },
+  {
+    "revision": "124108f409c23b86feb0",
+    "url": "/nhuang321/Taco-maker/static/js/runtime-main.cbf57dae.js"
+  }
+]);
